371ec6c8b03a20622096182e4427525b4d919e3c
wuyuewen <wuyuewen@github.com>
2023-11-11 23:47:50 +0800

    fix

--- scripts/ansible/install-env-centos7.sh
+++ scripts/ansible/install-env-centos7.sh
@@ -1,3 +1,2 @@
-ansible-galaxy collection install community.general
 ansible-playbook -i localhost, -c local playbooks/install_python_centos7.yml
 ansible-playbook -i localhost, -c local playbooks/install_packages_centos7.yml --tags=packages
