a3fa884aa0e7cdd78c9df7894cefea932ab08c8b
syswu <wuheng@iscas.ac.cn>
2023-12-14 20:37:19 +0800

    update dhcpid

--- ovnctl/plugins/dhcp.sh
+++ ovnctl/plugins/dhcp.sh
@@ -5,4 +5,7 @@ kubectl ko nbctl dhcp-options-create $cidr
 uuid=$(kubectl ko nbctl --bare --columns=_uuid find dhcp_options cidr="$cidr")
 
 ip=$(echo $cidr | awk -F'.' '{print$1"."$2"."$3".1"}')
-kubectl ko nbctl dhcp-options-set-options $uuid lease_time=3600 router=$ip server_id=$ip server_mac=c0:ff:ee:00:00:01
+kubectl ko nbctl dhcp-options-set-options $uuid lease_time=3600 router=$ip server_id=$ip server_mac=c0:ff:ee:00:00:01 ref=ovn-default
+
+#iface=$(ovs-vsctl get interface $name external_ids:iface-id | sed s/\"//g)
+#kubectl ko nbctl lsp-set-dhcpv4-options $iface $uuid
