db5ab79e990af1bdcf52a7176f0ae59baa10a40a
heyingyue <heyingyuelove@gmail.com>
2023-12-22 15:35:00 +0800

    solve pyinstaller error

--- scripts/shells/release-version-centos7.sh
+++ scripts/shells/release-version-centos7.sh
@@ -76,7 +76,7 @@ else
     echo "    Success compile <kubevmm-adm>."
 fi
 cp -f ./dist/centos7/kubevmm-adm ../../dist/centos7
-pyinstaller --distpath ./dist/centos7/ -F virshplus.py --hidden-import="tenacity"
+pyinstaller --distpath ./dist/centos7/ -F virshplus.py --hidden-import tenacity --add-data ../utils/*:utils
 if [ $? -ne 0 ]; then
     echo "    Failed to compile <virshplus>!"
     exit 1
